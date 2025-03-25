df280f1d72451aefe34e1b71b51a974499bccf82
Quincient Madding <quincientmadding@gmail.com>
2025-03-24 20:01:52 -0500

    Added script.js file and updated index.html

---
+++ script.js
@@ -0,0 +1,21 @@
+//referencing id name code output in html
+let output = document.getElementById("code-output");
+//printing the value of output in the console
+console.log("output = ", output);
+let x = "Quincient";
+//declaration and initialization of the variable with the value 3
+let y = 3 ;
+
+let z = true ;
+
+x = "John";
+//permanently set the variable name to logan
+const name = "Logan"
+//unable to change the name value to roman
+name = "roman" ;
+
+//declaration of age
+let age;
+//initialization of age
+age = 33;
+output.innerHTML= name;
