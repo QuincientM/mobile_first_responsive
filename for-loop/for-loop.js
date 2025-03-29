3b7067f73bd1593606e24bbc2e153df073f3905b
Quincient Madding <quincientmadding@gmail.com>
2025-03-28 20:19:49 -0500

    added for-loop

---
+++ for-loop/for-loop.js
@@ -0,0 +1,43 @@
+//referencing html elements
+let word = document.getElementById('word')
+let button = document.getElementById('button')
+let codeOutput = document.getElementById("code-output")
+
+//function definition
+
+function vowelRemover(value){
+    let solution = ''
+    console.log('value', value)
+    let len = value.length
+    console.log('the length of the number is',len)
+    for(let i = 0; i < len ;i++){
+        console.log(value[i])
+        if(value[i] == 'a' || value[i] == 'e' || value[i] == 'i' || value[i] == 'o' || value[i] == 'u' ){
+            
+        }
+        else{
+            solution += value[i]
+        }
+    }
+    codeOutput.innerHTML = solution
+
+}
+
+//add event listener to the button
+button.addEventListener('click', function(){
+    let wordValue = word.value
+    vowelRemover(wordValue)
+})
+
+/*
+
+this is a function definiton
+function name (perams) {
+    the action the function does
+}
+ this is how we do a function call
+ we use the function name followed by ()
+ name ()
+*/
+
+
